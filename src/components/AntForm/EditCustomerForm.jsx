ef57c1f57b1083cf7a077a60e75b55ff498818c7
Elif Nur Deniz <elinordeniz@gmail.com>
2024-02-01 13:16:06 +0300

    form little issue ffixed

--- src/components/AntForm/EditCustomerForm.jsx
+++ src/components/AntForm/EditCustomerForm.jsx
@@ -94,44 +94,7 @@ const EditCustomerForm = ({ onChange, fields, onFinish, editing, error, setError
             },
           ]}
         >
-            <Select
-    showSearch
-    style={{
-      width: 200,
-    }}
-    placeholder="Search to Select"
-    optionFilterProp="children"
-    filterOption={(input, option) => (option?.label ?? '').includes(input)}
-    filterSort={(optionA, optionB) =>
-      (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
-    }
-    options={[
-      {
-        value: '1',
-        label: 'Not Identified',
-      },
-      {
-        value: '2',
-        label: 'Closed',
-      },
-      {
-        value: '3',
-        label: 'Communicated',
-      },
-      {
-        value: '4',
-        label: 'Identified',
-      },
-      {
-        value: '5',
-        label: 'Resolved',
-      },
-      {
-        value: '6',
-        label: 'Cancelled',
-      },
-    ]}
-  />
+      <Input />
         </Form.Item>
 
         <Form.Item name="phone" label={t("phone")} >
