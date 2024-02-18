3a62ce0cd128cea3841e0f23e2fddd506bf5bae4
Elif Nur Deniz <elinordeniz@gmail.com>
2024-02-18 23:16:01 +0300

    alımlar tamamlandı, dummy graf ekl.

--- src/pages/MaterialStocks/index.jsx
+++ src/pages/MaterialStocks/index.jsx
@@ -1,12 +1,7 @@
-import Row from "@/components/Row";
 import Header from "./Header";
 
 import { useMemo, useState } from "react";
-import { useUser } from "@/store/hooks/user";
 
-import RawMaterialStocks from "@/pages/RawMaterialStocks";
-import RecipeMaterialStocks from "@/pages/RecipeMaterialStocks";
-import Stocks from "../Stocks";
 import Logs from "./Logs";
 
 const MaterialStocks = () => {
